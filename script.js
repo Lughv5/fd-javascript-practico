7658d9071c35d28c3b1c7cd0e581792837d1e02c
lughv5 <lughv5@hotmail.com>
2022-12-09 17:00:17 -0500

    se fusiona menu de mobile

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const menuEmail = document.querySelector(".navbar-email")
 const deskMenu = document.querySelector(".desktop-menu")
+const hamIcon = document.querySelector(".menu")
+const mobMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toggleDeskMenu)
-function toggleDeskMenu(menu) {
+hamIcon.addEventListener("click", toggleMobMenu)
+function toggleDeskMenu() {
     deskMenu.classList.toggle("inactive")
+}
+function toggleMobMenu() {
+    mobMenu.classList.toggle("inactive")
 }
